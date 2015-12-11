677af74abbf03343789697f2368bc625f26ac772
compay2k <compay2k@gmail.com>
2015-12-11 10:41:42 +0100

    test

---
+++ regions.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-type: application/json; charset=utf-8');
+
+//sleep(2);
+
+echo '{"regions": [
+		{ "id":"fc", "nom":"Franche Comte" },
+		{ "id":"idf", "nom":"Ile de France" },
+		{ "id":"paca", "nom":"Provence Alpes Côte d\'azur" },
+		{ "id":"bretagne", "nom":"Bretagne" },
+		{ "id":"bourgogne", "nom":"Bourgogne" }
+	]}';
+
+?>
